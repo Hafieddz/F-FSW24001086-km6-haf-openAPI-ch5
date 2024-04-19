c29caf3bfdd196e98f5a140312ceb46343150245
Hafieddz <hasmy41@gmail.com>
2024-04-19 17:37:52 +0800

    feat : create middleware for authorization

---
+++ middleware/authorization.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const Auth = require("../models/Auth");
+
+const authorization = async (req, res, next) => {
+  try {
+    const bearerToken = req.headers.authorization;
+
+    console.log(bearerToken);
+    if (!bearerToken) throw new Error("Missing token...");
+
+    const token = bearerToken.split(" ")[1];
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    const user = await User.findById(payload.id);
+
+    if (user === null) throw new Error("Akun tidak ada...");
+
+    req.user = user;
+
+    console.log("Continue to the controller");
+    next();
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: error.message,
+    });
+  }
+};
+
+module.exports = authorization;
