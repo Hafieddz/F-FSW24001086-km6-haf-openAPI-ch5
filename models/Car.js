1103261379c9e7c50a3eca25f9cfb416bc2489c1
Hafieddz <hasmy41@gmail.com>
2024-04-21 13:54:25 +0800

    feat : add isAvailble field

--- models/Car.js
+++ models/Car.js
@@ -12,7 +12,11 @@ const carSchema = new mongoose.Schema({
   },
   image: {
     type: String,
-    required : true
+    required: true,
+  },
+  isAvailable: {
+    type: Boolean,
+    default: true,
   },
   createdAt: {
     type: Date,
