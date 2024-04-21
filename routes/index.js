018201158b935a4340eab2a63680b32316f46c88
Hafieddz <hasmy41@gmail.com>
2024-04-21 13:55:22 +0800

    feat : add routes for api-docs

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const router = require("express").Router();
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocs = require("../docs/api-docs.json");
+const swaggerExample = require("../docs/example.json");
+
 const carRoutes = require("./carRoutes");
 const userRoutes = require("./userRoutes");
 const authRoutes = require("./authRoutes");
 
+router.use("/api_docs", swaggerUI.serve);
+router.use("/api_docs", swaggerUI.setup(swaggerDocs));
+
 router.use("/api/v1/cars", carRoutes);
 router.use("/api/v1/users", userRoutes);
-router.use("/api/v1/auth", authRoutes)
+router.use("/api/v1/auth", authRoutes);
 
 module.exports = router;
